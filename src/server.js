bb0555ce2d1d15b3cbc488f42d5d46e5ca228c27
mrilea <marlinrilea@gmail.com>
2022-03-28 22:34:52 -0700

    test

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const { PORT = 5000 } = process.env;
 const app = require("./app");
 const knex = require("./db/connection");
 
+app.set('port', process.env.PORT || 5000);
+
 const listener = () => console.log(`Listening on Port ${PORT}!`);
 
 knex.migrate
